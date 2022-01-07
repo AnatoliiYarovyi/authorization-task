b5304f78dc99c798fe8a1c4890ed8c0854546d46
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-01-07 13:32:18 +0200

    Adds controllers

---
+++ controllers/auth/me.js
@@ -0,0 +1,36 @@
+const { Unauthorized } = require('http-errors')
+
+const TokenService = require('../../service/token-service')
+
+const me = async (req, res) => {
+  const users = req.app.locals.collection
+  const collectionToken = req.app.locals.collectionToken
+  const { refreshToken } = req.cookies
+  if (!refreshToken) {
+    throw new Unauthorized('refreshToken error')
+  }
+  const userDataRefresh = await TokenService.validateRefreshToken(refreshToken)
+  const tokenFromDb = await TokenService.findToken(
+    collectionToken,
+    refreshToken,
+  )
+  if (!userDataRefresh || !tokenFromDb) {
+    throw new Unauthorized('userDataRefresh || tokenFromDb - error')
+  }
+  const user = await users.findOne({ _id: tokenFromDb.user })
+  if (!user) {
+    throw new Unauthorized('user - error')
+  }
+  const userDataAccess = await TokenService.validateAccessToken(
+    user.accessToken,
+  )
+  if (!userDataAccess) {
+    throw new Unauthorized('accessToken - error')
+  }
+
+  res.json({
+    userEmail: user.email,
+  })
+}
+
+module.exports = me
