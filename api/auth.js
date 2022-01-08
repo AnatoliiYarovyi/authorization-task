e550c559db9afcf8ee1a3e9c3bfa3aed8d1655cb
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-01-08 15:15:27 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,12 +5,8 @@ const { auth: ctrl } = require('../controllers')
 const { controllerWrapper } = require('../middlewares')
 
 router.post('/signup', controllerWrapper(ctrl.signup))
-
 router.post('/login', controllerWrapper(ctrl.login))
-router.get(
-  '/me',
-controllerWrapper(ctrl.me)
-)
 router.post('/refresh', controllerWrapper(ctrl.refresh))
+router.get(`/me`, controllerWrapper(ctrl.me))
 
 module.exports = router
